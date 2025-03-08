b0953642f8409d76a4c2b3cf412e54e84eb08711
Joshua Davies <cubchoo.moo@gmail.com>
2025-03-08 14:29:57 +1300

    modified:   final1.py

--- final1.py
+++ final1.py
@@ -1,13 +1,24 @@
 import pandas as pd
+import datetime
 def main():
     timetable = pd.read_csv('final/mrs_wilde.csv')
     day , period = ask_info()
+    
     display_location(day,period,timetable)
     
 def display_location(day,period,timetable):
-    print(timetable.iloc[(period-1)*3:(period-1)*3+3]["day "+day])
-
-        
+    teacherDetails = timetable.iloc[(period-1)*3:(period-1)*3+3]["day "+day].values
+    print(f"Teacher's Location is {teacherDetails[2]}")
+    print(f"Teacher's Class is {teacherDetails[0]}")
+    print(f"Teacher's Class code is {teacherDetails[1]}")
+    
+def get_datetime():
+    currentDateAndTime = datetime.datetime.now()
+    currentDate = currentDateAndTime.strftime("%d/%m/%Y")
+    currentTime = currentDateAndTime.strftime("%H:%M:%S")
+    print(currentDate,currentTime)
+    
+    
 
 def ask_info():
     day=input("Day> ")
